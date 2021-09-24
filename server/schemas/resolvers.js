37b0d30679573d72d02f687ddf63f6a63c7cf108
Buney <dpkinser93@gmail.com>
2021-09-23 20:49:52 -0600

    added files for typeDefs and resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,9 @@
+const { Book, User } = require('../models');
+
+const resolvers = {
+    Query: {
+        something: 0,
+    }
+}
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,7 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    queries go here
+`;
+
+module.exports = typeDefs;
