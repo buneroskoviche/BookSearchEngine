9ba02508925e5475b76c0fb5a9034d0780c32737
Buney <dpkinser93@gmail.com>
2021-09-23 20:56:38 -0600

    added typeDefs for models

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,22 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    queries go here
+    type Book {
+        _id
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type User {
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]
+    }
 `;
 
 module.exports = typeDefs;
